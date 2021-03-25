d8dcf2667149fb2211ad112bfe0df4042492d8cb
swatkat1127 <swatkat1127@gmail.com>
2021-03-25 12:25:55 +0530

    refactoring challenge : file named roundedbutton

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'registration_screen.dart';
 import 'package:flutter/animation.dart';
+import 'package:flash_chat/Components/RoundedButton.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static String routeName = "/welcome";
@@ -58,45 +59,16 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginScreen.routeName);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.routeName);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
-            ),
+            RoundedButton(color: Colors.lightBlueAccent,title: 'log in', onpressed: (){
+              Navigator.pushNamed(context, LoginScreen.routeName);
+            },),
+            RoundedButton(color: Colors.blueAccent,title: 'Register',onpressed:(){
+              Navigator.pushNamed(context, RegistrationScreen.routeName);
+            },),
           ],
         ),
       ),
     );
   }
 }
+
